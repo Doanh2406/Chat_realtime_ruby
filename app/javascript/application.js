53002e174e1c18a4d9709b5b4a92d4b0cf1f6585
quydi <quocdoanh02468@gmail.com>
2022-11-27 21:55:48 +0700

    fix conflict

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+import "jquery_ujs"
 //= require jquery
 //= require jquery_ujs
 //= require turbo-link
 //= require popper
 //= require bootstrap-sprockets
 //= require tree .
+
