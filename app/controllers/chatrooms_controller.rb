05507ec9949ee0304baf81d64eac2b3642c535cc
quydi <quocdoanh02468@gmail.com>
2022-11-27 21:57:30 +0700

    create conflix for fun

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -8,6 +8,7 @@ class ChatroomsController < ApplicationController
 
   # GET /chatrooms/1 or /chatrooms/1.json
   def show
+		show messha
 		@messages = @chatroom.messages.order(created_at: :asc).limit(50)
   end
 
